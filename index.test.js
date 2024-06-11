5dc9b3fb28a8c933858bd6a379299db75c92e358
unicornGL <s941407@gmail.com>
2024-06-12 01:45:11 +0800

    bubble sort test

--- index.test.js
+++ index.test.js
@@ -1,4 +1,5 @@
 import selectionSort from './selectionSort'
+import bubbleSort from './bubbleSort'
 
 describe('selectionSort test', () => {
     test('empty array', () => {
@@ -30,5 +31,38 @@ describe('selectionSort test', () => {
         const sortedArray = [...largeArray].sort((a, b) => a - b)
 
         expect(selectionSort(largeArray)).toEqual(sortedArray)
+    })
+})
+
+describe('bubbleSort test', () => {
+    test('empty array', () => {
+        expect(bubbleSort([])).toEqual([])
+    })
+
+    test('single number array', () => {
+        expect(bubbleSort([5])).toEqual([5])
+    })
+
+    test('sorted array', () => {
+        expect(bubbleSort([1, 2, 3, 4, 5])).toEqual([1, 2, 3, 4, 5])
+    })
+
+    test('reversed sorted array', () => {
+        expect(bubbleSort([5, 4, 3, 2, 1])).toEqual([1, 2, 3, 4, 5])
+    })
+
+    test('array w/ duplicate numbers', () => {
+        expect(bubbleSort([3, 1, 4, 1, 5, 9, 2, 6, 5])).toEqual([1, 1, 2, 3, 4, 5, 5, 6, 9])
+    })
+
+    test('array w/ negative numbers', () => {
+        expect(bubbleSort([-3, 0, 1, -10, 5])).toEqual([-10, -3, 0, 1, 5])
+    })
+
+    test('large array', () => {
+        const largeArray = Array.from({ length: 10000 }, () => Math.floor(Math.random() * 10000))
+        const sortedArray = [...largeArray].sort((a, b) => a - b)
+
+        expect(bubbleSort(largeArray)).toEqual(sortedArray)
     })
 })
