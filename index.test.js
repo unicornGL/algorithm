16c6b4d3a8606c84f56de0745d90b0820e2a11b1
unicornGL <s941407@gmail.com>
2024-06-16 02:03:08 +0800

    merge sort test

--- index.test.js
+++ index.test.js
@@ -1,6 +1,7 @@
 import selectionSort from './selectionSort'
 import bubbleSort from './bubbleSort'
 import insertionSort from './insertionSort'
+import mergeSort from './mergeSort'
 
 describe('selectionSort test', () => {
     test('empty array', () => {
@@ -98,5 +99,38 @@ describe('insertionSort test', () => {
         const sortedArray = [...largeArray].sort((a, b) => a - b)
 
         expect(insertionSort(largeArray)).toEqual(sortedArray)
+    })
+})
+
+describe('mergeSort test', () => {
+    test('empty array', () => {
+        expect(mergeSort([])).toEqual([])
+    })
+
+    test('single number array', () => {
+        expect(mergeSort([5])).toEqual([5])
+    })
+
+    test('sorted array', () => {
+        expect(mergeSort([1, 2, 3, 4, 5])).toEqual([1, 2, 3, 4, 5])
+    })
+
+    test('reversed sorted array', () => {
+        expect(mergeSort([5, 4, 3, 2, 1])).toEqual([1, 2, 3, 4, 5])
+    })
+
+    test('array w/ duplicate numbers', () => {
+        expect(mergeSort([3, 1, 4, 1, 5, 9, 2, 6, 5])).toEqual([1, 1, 2, 3, 4, 5, 5, 6, 9])
+    })
+
+    test('array w/ negative numbers', () => {
+        expect(mergeSort([-3, 0, 1, -10, 5])).toEqual([-10, -3, 0, 1, 5])
+    })
+
+    test('large array', () => {
+        const largeArray = Array.from({ length: 10000 }, () => Math.floor(Math.random() * 10000))
+        const sortedArray = [...largeArray].sort((a, b) => a - b)
+
+        expect(mergeSort(largeArray)).toEqual(sortedArray)
     })
 })
