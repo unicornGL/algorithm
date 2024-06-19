f66a60b4d8a0e7fba8a26049c0968f0ce99407dd
unicornGL <s941407@gmail.com>
2024-06-20 00:06:02 +0800

    simple quick sort

---
+++ simpleQuickSort.js
@@ -0,0 +1,24 @@
+/**
+ * Simple Quick Sort
+ * -> Recursively set a pivot, put numbers smaller than pivot in left array and numbers larger than pivot in right array
+ * Time Complexity = O(n log n), but Space Complexity is larger than in-place version
+ */
+
+export default simpleQuickSort = (arr) => {
+    const length = arr.length
+
+    if (length <= 1) return arr
+
+    let pivot = arr[0]
+    let leftArr = [], rightArr = []
+
+    for (let i = 1; i < length; i++) {
+        if (arr[i] < pivot) {
+            leftArr.push(arr[i])
+        } else {
+            rightArr.push(arr[i])
+        }
+    }
+
+    return [...simpleQuickSort(leftArr), pivot, ...simpleQuickSort(rightArr)]
+}
