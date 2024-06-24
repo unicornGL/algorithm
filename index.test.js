262b1a74df25447ab2190ddcbb245eed146731a2
unicornGL <s941407@gmail.com>
2024-06-25 01:38:07 +0800

    counting sort test

--- index.test.js
+++ index.test.js
@@ -6,6 +6,7 @@ import simpleQuickSort from './simpleQuickSort'
 import inPlaceQuickSort from './inPlaceQuickSort'
 import randomQuickSort from './randomQuickSort'
 import heapSort from './heapSort'
+import countingSort from './countingSort'
 
 describe('bubbleSort test', () => {
     test('empty array', () => {
@@ -268,5 +269,38 @@ describe('heapSort test', () => {
         const sortedArray = [...largeArray].sort((a, b) => a - b)
 
         expect(heapSort(largeArray)).toEqual(sortedArray)
+    })
+})
+
+describe('countingSort test', () => {
+    test('empty array', () => {
+        expect(countingSort([])).toEqual([])
+    })
+
+    test('single number array', () => {
+        expect(countingSort([5])).toEqual([5])
+    })
+
+    test('sorted array', () => {
+        expect(countingSort([1, 2, 3, 4, 5])).toEqual([1, 2, 3, 4, 5])
+    })
+
+    test('reversed sorted array', () => {
+        expect(countingSort([5, 4, 3, 2, 1])).toEqual([1, 2, 3, 4, 5])
+    })
+
+    test('array w/ duplicate numbers', () => {
+        expect(countingSort([3, 1, 4, 1, 5, 9, 2, 6, 5])).toEqual([1, 1, 2, 3, 4, 5, 5, 6, 9])
+    })
+
+    test('array w/ negative numbers', () => {
+        expect(countingSort([-3, 0, 1, -10, 5])).toEqual([-10, -3, 0, 1, 5])
+    })
+
+    test('large array', () => {
+        const largeArray = Array.from({ length: 10000 }, () => Math.floor(Math.random() * 10000))
+        const sortedArray = [...largeArray].sort((a, b) => a - b)
+
+        expect(countingSort(largeArray)).toEqual(sortedArray)
     })
 })
